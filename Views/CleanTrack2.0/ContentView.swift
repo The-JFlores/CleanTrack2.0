83ecf5c3f960ead38c27adf3f3c2c5950b3cb522
JoseFlores <joseolmedo.florespaniagua@triosstudent.com>
2026-01-09 10:24:20 -0500

    Add LoginView whith email/password fields and FirebaseAuth integration.

---
+++ Views/CleanTrack2.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CleanTrack2.0
+//
+//  Created by Jose Flores on 2026-01-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Views/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  CleanTrack2.0
+//
+//  Created by Jose Flores on 2026-01-09.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
